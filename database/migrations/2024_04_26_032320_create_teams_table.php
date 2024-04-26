dd031fc8ce8530a0748cdaca2a8e847c3375b20c
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2024-04-26 13:32:04 +0700

    memperbaiki migration

--- database/migrations/2024_04_26_032320_create_teams_table.php
+++ database/migrations/2024_04_26_032320_create_teams_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('company_id')->unsigned();
             $table->string('name');
             $table->string('icon');
-            $table->bigInteger('company_id')->unsigned();
             $table->softDeletes();
             $table->timestamps();
         });
--- database/migrations/2024_04_26_032442_create_roles_table.php
+++ database/migrations/2024_04_26_032442_create_roles_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->bigInteger('company_id')->unsigned();
+            $table->string('name');
             $table->softDeletes();
             $table->timestamps();
         });
