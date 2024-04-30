e515a0e151d2547fa85f7f0943e9f76876e9b452
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2024-04-30 10:58:20 +0700

    menambahkan helper respon api jika memiliki halaman lebih dari 1

---
+++ app/Helpers/ResponseApiPaginate.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Helpers;
+
+class ResponApiFormatter
+{
+    protected static $response = [
+        'meta' => [
+            'code' => 200,
+            'status' => 'success',
+            'message' => null,
+        ],
+        'data' => null,
+    ];
+
+    /** Response ketika success */
+    public static function success($data = null, $message = null)
+    {
+        self::$response['meta']['message'] = $message;
+        self::$response['data'] = $data;
+
+        if ($data->current_page) {
+            self::$response['data'] = $data->data;
+            unset($data->data);
+            self::$response['pagination'] = $data;
+        }
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+
+    /** Response ketika gagal */
+    public static function error($data = null, $message = null, $code = 400)
+    {
+        self::$response['meta']['status'] = 'error';
+        self::$response['meta']['code'] = $code;
+        self::$response['meta']['message'] = $message;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+}
