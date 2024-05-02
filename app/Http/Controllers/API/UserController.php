6776eab689687c525c114c1e8111c3cf1be5c5b2
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2024-05-02 14:25:05 +0700

    membuat api user

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,6 +9,10 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Validator;
+
 
 class UserController extends Controller
 {
@@ -18,15 +22,10 @@ class UserController extends Controller
             /** Validate Request */
             $request->validate([
                 'email' => 'required|email',
-                'email' => 'password'
+                'password' => 'string'
             ]);
 
             /** Find User by email */
-            $credentials = $request(['email', 'password']);
-            if (!Auth::attempt($credentials)) {
-                return ResponApiFormatter::error('Unauthorized', 401);
-            }
-
             $user = User::where('email', $request->email)->first();
             if (!Hash::check($request->password, $user->password)) {
                 throw new Exception('Invalid password');
@@ -42,16 +41,70 @@ class UserController extends Controller
                 'user' => $user,
             ], 'Login Success.');
         } catch (Exception $e) {
+            /** Return Response Error*/
             return ResponApiFormatter::error('Authentication Failed');
         }
     }
-    public function register()
+
+    public function register(Request $request)
     {
+        try {
+            /** Validate request */
+            $validator = Validator::make($request->all(), [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => [
+                    'string',
+                    Password::min(size: 8)
+                        ->letters()
+                        ->numbers()
+                        ->symbols()
+                        ->uncompromised()
+                ]
+            ]);
+
+            /** Response Validate fails */
+            if ($validator->fails()) {
+                return ResponApiFormatter::error(['message' => $validator->errors()], 422);
+            }
+            // dd($request->all(), htmlspecialchars($request->name));
+
+            /** Create user */
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            /** Generate token */
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            /** Return Response */
+            return ResponApiFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ], 'Register Success');
+        } catch (Exception $error) {
+            /** Return Response Error */
+            return ResponApiFormatter::error($error->getMessage());
+        }
     }
-    public function logout()
+
+    public function logout(Request $request)
     {
+        /** Revoke Token */
+        $token = $request->user()->currentAccessToken()->delete();
+
+        /** Return Response */
+        return ResponApiFormatter::success($token, 'Logout Success');
     }
-    public function fetch()
+    public function fetch(Request $request)
     {
+        /** Get User */
+        $user = $request->user();
+
+        /** Return Response */
+        return ResponApiFormatter::success($user, 'Fetch Success');
     }
 }
